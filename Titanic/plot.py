6f9ca9c5121bf4d38dc025e6470f2977f5d91c78
wangpan <wangpan233@gmail.com>
2018-07-28 15:46:34 +0800

    Add model seaborn to plot

--- Titanic/plot.py
+++ Titanic/plot.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[7]:
+# In[19]:
 
 
 import pandas as pd
@@ -9,6 +9,7 @@ import numpy as np
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 from pylab import mpl
+import seaborn as sns
 from sklearn.cross_validation import train_test_split
 mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
@@ -25,6 +26,145 @@ from sklearn.pipeline import Pipeline
 pipe_titanic = Pipeline([
                         ('clf',LogisticRegression(C=10.0,penalty='l2',tol=1e-6))])
 pipe_titanic.fit(X_train,y_train)
+
+
+# In[5]:
+
+
+data['Survived'].value_counts().plot.pie(autopct = '%1.2f%%')
+plt.axis('equal')
+
+
+# In[33]:
+
+
+ax = sns.violinplot('Sex','Age',data=data,hue='Survived',split=True)
+ax.set_yticks(range(0, 110, 10))
+ax.grid()
+
+
+# In[41]:
+
+
+ax = sns.countplot(x='Pclass',hue='Survived',data=data)
+
+
+# In[36]:
+
+
+fig = plt.figure(figsize=figsize)
+fig.set(alpha=0.65) # 设置图像透明度，无所谓
+plt.title(u"根据舱等级和性别的获救情况")
+data_train = data
+
+ax1=fig.add_subplot(141)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass != 3].value_counts().plot(kind='bar', label="female highclass", color='#FA2479')
+ax1.set_xticklabels([u"获救", u"未获救"], rotation=0)
+ax1.legend([u"女性/高级舱"], loc='best')
+
+ax2=fig.add_subplot(142, sharey=ax1)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='female, low class', color='pink')
+ax2.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"女性/低级舱"], loc='best')
+
+ax3=fig.add_subplot(143, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass != 3].value_counts().plot(kind='bar', label='male, high class',color='lightblue')
+ax3.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/高级舱"], loc='best')
+
+ax4=fig.add_subplot(144, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='male low class', color='steelblue')
+ax4.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/低级舱"], loc='best')
+
+
+
+plt.show()
+
+
+# In[12]:
+
+
+data['Age'].hist(bins=70)
+plt.xlabel('Age')
+plt.ylabel('Num')
+
+
+# In[37]:
+
+
+from pylab import mpl
+mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=figsize)
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
+data_train.Survived.value_counts().plot(kind='bar')# 柱状图 
+plt.title(u"获救情况 (1为获救)") # 标题
+plt.ylabel(u"人数")  
+
+plt.subplot2grid((2,3),(0,1))
+data_train.Pclass.value_counts().plot(kind="bar")
+plt.ylabel(u"人数")
+plt.title(u"乘客等级分布")
+
+plt.subplot2grid((2,3),(0,2))
+plt.scatter(data_train.Survived, data_train.Age)
+plt.ylabel(u"年龄")                         # 设定纵坐标名称
+plt.grid(b=True, which='major', axis='y') 
+plt.title(u"按年龄看获救分布 (1为获救)")
+
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+data_train.Age[data_train.Pclass == 1].plot(kind='kde')   
+data_train.Age[data_train.Pclass == 2].plot(kind='kde')
+data_train.Age[data_train.Pclass == 3].plot(kind='kde')
+plt.xlabel(u"年龄")# plots an axis lable
+plt.ylabel(u"密度") 
+plt.title(u"各等级的乘客年龄分布")
+plt.legend((u'头等舱', u'2等舱',u'3等舱'),loc='best') # sets our legend for our graph.
+
+
+# In[39]:
+
+
+Survived_1 = data_train.Embarked[data_train.Survived == 1].value_counts()
+Survived_0 = data_train.Embarked[data_train.Survived == 0].value_counts()
+x = np.arange(3)
+y1 = np.array(list(Survived_1))
+y2 = np.array(list(Survived_0+Survived_1))
+tdf = pd.DataFrame({u'获救':Survived_1,u'未获救':Survived_0})
+tdf.plot(kind='bar',stacked=True)
+for a,b in zip(x,y1):
+    plt.text(a,b+0.05,'%.f' % b, ha = 'center',va = 'bottom',fontsize=10)
+for a,b in zip(x,y2):
+    plt.text(a,b+0.05,'%.f' % b, ha = 'center',va = 'bottom',fontsize=10)
+plt.title(u"各登船口岸上船人数")
+plt.ylabel(u"人数")  
+plt.show()
+
+
+# In[11]:
+
+
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_0 = data_train.Pclass[data_train.Survived == 0].value_counts()
+Survived_1 = data_train.Pclass[data_train.Survived == 1].value_counts()
+df=pd.DataFrame({u'获救':Survived_1, u'未获救':Survived_0})
+df.plot(kind='bar', stacked=True)
+x = np.arange(3)
+y = np.array(list(Survived_0+Survived_1))
+for a,b in zip(x,y):
+    plt.text(a, b+0.05, '%.0f' % b, ha='center', va= 'bottom',fontsize=7)
+plt.grid(True)
+plt.title(u"各乘客等级的获救情况")
+plt.xlabel(u"乘客等级") 
+plt.ylabel(u"人数") 
+plt.show()
 
 
 # In[8]:
@@ -82,125 +222,5 @@ plt.xlabel('Parameter C')
 plt.ylabel('Accuracy')
 plt.legend(loc = "upper right")
 plt.ylim([0.6,1.0])
-plt.show()
-
-
-# In[9]:
-
-
-fig = plt.figure(figsize=figsize)
-fig.set(alpha=0.65) # 设置图像透明度，无所谓
-plt.title(u"根据舱等级和性别的获救情况")
-data_train = data
-
-ax1=fig.add_subplot(141)
-data_train.Survived[data_train.Sex == 'female'][data_train.Pclass != 3].value_counts().plot(kind='bar', label="female highclass", color='#FA2479')
-ax1.set_xticklabels([u"获救", u"未获救"], rotation=0)
-ax1.legend([u"女性/高级舱"], loc='best')
-
-ax2=fig.add_subplot(142, sharey=ax1)
-data_train.Survived[data_train.Sex == 'female'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='female, low class', color='pink')
-ax2.set_xticklabels([u"未获救", u"获救"], rotation=0)
-plt.legend([u"女性/低级舱"], loc='best')
-
-ax3=fig.add_subplot(143, sharey=ax1)
-data_train.Survived[data_train.Sex == 'male'][data_train.Pclass != 3].value_counts().plot(kind='bar', label='male, high class',color='lightblue')
-ax3.set_xticklabels([u"未获救", u"获救"], rotation=0)
-plt.legend([u"男性/高级舱"], loc='best')
-
-ax4=fig.add_subplot(144, sharey=ax1)
-data_train.Survived[data_train.Sex == 'male'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='male low class', color='steelblue')
-ax4.set_xticklabels([u"未获救", u"获救"], rotation=0)
-plt.legend([u"男性/低级舱"], loc='best')
-
-
-
-plt.show()
-
-
-# In[42]:
-
-
-data['Survived'].value_counts().plot.pie(autopct = '%1.2f%%')
-plt.figure(figsize=(90,5))
-a = data['Age'].value_counts()
-data['Age'].hist(bins=a.size)
-a.sort_index()
-for c,b in zip(a.index,a.values):
-    plt.text(c,b+0.05,'%.f' % b, ha = 'center',va = 'bottom',fontsize=10)
-
-
-# In[10]:
-
-
-from pylab import mpl
-mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
-mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
-import matplotlib.pyplot as plt
-fig = plt.figure(figsize=figsize)
-fig.set(alpha=0.2)  # 设定图表颜色alpha参数
-
-plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
-data_train.Survived.value_counts().plot(kind='bar')# 柱状图 
-plt.title(u"获救情况 (1为获救)") # 标题
-plt.ylabel(u"人数")  
-
-plt.subplot2grid((2,3),(0,1))
-data_train.Pclass.value_counts().plot(kind="bar")
-plt.ylabel(u"人数")
-plt.title(u"乘客等级分布")
-
-plt.subplot2grid((2,3),(0,2))
-plt.scatter(data_train.Survived, data_train.Age)
-plt.ylabel(u"年龄")                         # 设定纵坐标名称
-plt.grid(b=True, which='major', axis='y') 
-plt.title(u"按年龄看获救分布 (1为获救)")
-
-
-plt.subplot2grid((2,3),(1,0), colspan=2)
-data_train.Age[data_train.Pclass == 1].plot(kind='kde')   
-data_train.Age[data_train.Pclass == 2].plot(kind='kde')
-data_train.Age[data_train.Pclass == 3].plot(kind='kde')
-plt.xlabel(u"年龄")# plots an axis lable
-plt.ylabel(u"密度") 
-plt.title(u"各等级的乘客年龄分布")
-plt.legend((u'头等舱', u'2等舱',u'3等舱'),loc='best') # sets our legend for our graph.
-
-
-plt.subplot2grid((2,3),(1,2))
-Survived_1 = data_train.Embarked[data_train.Survived == 1].value_counts()
-Survived_0 = data_train.Embarked[data_train.Survived == 0].value_counts()
-x = np.arange(3)
-y1 = np.array(list(Survived_1))
-y2 = np.array(list(Survived_0+Survived_1))
-tdf = pd.DataFrame({u'获救':Survived_1,u'未获救':Survived_0})
-tdf.plot(kind='bar',stacked=True)
-for a,b in zip(x,y1):
-    plt.text(a,b+0.05,'%.f' % b, ha = 'center',va = 'bottom',fontsize=10)
-for a,b in zip(x,y2):
-    plt.text(a,b+0.05,'%.f' % b, ha = 'center',va = 'bottom',fontsize=10)
-plt.title(u"各登船口岸上船人数")
-plt.ylabel(u"人数")  
-plt.show()
-
-
-# In[11]:
-
-
-fig = plt.figure()
-fig.set(alpha=0.2)  # 设定图表颜色alpha参数
-
-Survived_0 = data_train.Pclass[data_train.Survived == 0].value_counts()
-Survived_1 = data_train.Pclass[data_train.Survived == 1].value_counts()
-df=pd.DataFrame({u'获救':Survived_1, u'未获救':Survived_0})
-df.plot(kind='bar', stacked=True)
-x = np.arange(3)
-y = np.array(list(Survived_0+Survived_1))
-for a,b in zip(x,y):
-    plt.text(a, b+0.05, '%.0f' % b, ha='center', va= 'bottom',fontsize=7)
-plt.grid(True)
-plt.title(u"各乘客等级的获救情况")
-plt.xlabel(u"乘客等级") 
-plt.ylabel(u"人数") 
 plt.show()
 
